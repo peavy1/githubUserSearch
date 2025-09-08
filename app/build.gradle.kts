e43f457b489d855e137fb1e11ec9a8fd2b01d287
peavy1 <peavy@culturehero.net>
2025-09-08 15:36:28 +0900

    feat: 검색창 debounce - 코틀린 코루틴 flow의 debounce로 변경

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -84,7 +84,7 @@ dependencies {
     implementation("com.google.dagger:hilt-android:2.51.1")
     ksp("com.google.dagger:hilt-compiler:2.51.1")
 
-    implementation ("com.jakewharton.rxbinding4:rxbinding:4.0.0")
+//    implementation ("com.jakewharton.rxbinding4:rxbinding:4.0.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.3.0")
--- app/src/main/java/com/hater/githubsearch/viewmodel/GithubSearchViewModel.kt
+++ app/src/main/java/com/hater/githubsearch/viewmodel/GithubSearchViewModel.kt
@@ -3,20 +3,20 @@ package com.hater.githubsearch.viewmodel
 import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import androidx.paging.Pager
-import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
-import com.hater.githubsearch.model.GithubUser
 import com.hater.githubsearch.model.UserInfo
-import com.hater.githubsearch.repository.SearchPagingSource
 import com.hater.githubsearch.repository.SearchRepository
-import com.hater.githubsearch.util.Constants.PAGING_SIZE
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.collectLatest
+import kotlinx.coroutines.flow.debounce
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.filter
+import kotlinx.coroutines.flow.stateIn
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -25,8 +25,24 @@ class GithubSearchViewModel @Inject constructor(
     private val searchRepository: SearchRepository
 ): ViewModel() {
 
+    private val keyword = MutableStateFlow("")
+
     private val _searchPagingResult = MutableStateFlow<PagingData<UserInfo>>(PagingData.empty())
     val searchPagingResult: StateFlow<PagingData<UserInfo>> = _searchPagingResult.asStateFlow()
+
+    val debouncedKeyword: StateFlow<String> = keyword
+        .debounce(2000L)
+        .filter { it.isNotEmpty() }
+        .distinctUntilChanged()
+        .stateIn(
+            scope = viewModelScope,
+            started = SharingStarted.WhileSubscribed(5000),
+            initialValue = ""
+        )
+
+    fun updateKeyword(text: String) {
+        keyword.value = text
+    }
 
     fun searchUser(query: String) {
         viewModelScope.launch {
